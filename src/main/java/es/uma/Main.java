2d5aaea296e449fb586711c5a8f35a99c135f5b8
Andrei <141495654+n3k1ng@users.noreply.github.com>
2024-11-13 21:08:03 +0100

    Add first IA model interaction

--- src/main/java/es/uma/Main.java
+++ src/main/java/es/uma/Main.java
@@ -1,7 +1,19 @@
 package es.uma;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String apiKey = "demo";
+
+        // Create an instance of OpenAI
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .apiKey(apiKey)
+                .modelName("gpt-4o-mini")
+                .build();
+
+        String answer = model.generate("Say 'Hello World'");
+        System.out.println(answer);
+
     }
 }
