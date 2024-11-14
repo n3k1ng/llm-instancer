dbb1fb6dcdd2ffda217e58fbb8c07a7161bf65a1
Andrei <141495654+n3k1ng@users.noreply.github.com>
2024-11-14 14:54:41 +0100

    Basic Gemini integration and template message replace

--- src/main/java/es/uma/Main.java
+++ src/main/java/es/uma/Main.java
@@ -1,19 +1,68 @@
 package es.uma;
 
-import dev.langchain4j.model.openai.OpenAiChatModel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.SystemMessage;
 
 public class Main {
+
+    private static String loadToString(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + filePath);
+            return "";
+        }
+    }
+
+    private static String loadTemplate() {
+        String messageTemplate = loadToString(".\\src\\main\\resources\\messageTemplate.txt");
+        String diagram = loadToString(".\\src\\main\\resources\\diagram.use");
+        String example = loadToString(".\\src\\main\\resources\\example.soil");
+        
+        // Consider StringBuilder if multiple replaces //
+        messageTemplate = messageTemplate.replace("{diagram}", diagram);
+        messageTemplate = messageTemplate.replace("{example}", example);
+
+        return messageTemplate;
+    }
+
+    private static void saveInstance(String response) {
+        try (FileWriter writer = new FileWriter(".\\src\\main\\resources\\instance.soil")){
+            writer.write(response);
+            System.out.println("Response saved to instance.soil");
+        } catch (Exception e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+    
+    interface ModelInstantiator {     
+        @SystemMessage(fromResource = "systemMessage.txt")
+        String chat(String userMessage);
+    }
+
     public static void main(String[] args) {
-        String apiKey = "demo";
 
-        // Create an instance of OpenAI
-        OpenAiChatModel model = OpenAiChatModel.builder()
-                .apiKey(apiKey)
-                .modelName("gpt-4o-mini")
-                .build();
+        ChatLanguageModel gemini = GoogleAiGeminiChatModel.builder()
+        .apiKey(System.getenv("GEMINI_KEY"))
+        .modelName("gemini-1.5-flash")
+        .build();
 
-        String answer = model.generate("Say 'Hello World'");
-        System.out.println(answer);
+        String userMessage = loadTemplate();
+
+        ModelInstantiator modelInstantiator = AiServices.create(ModelInstantiator.class, gemini);
+        String response = modelInstantiator.chat(userMessage);
+
+        System.out.println("Response:");
+        System.out.println(response);
+        saveInstance(response);
 
     }
+
 }
