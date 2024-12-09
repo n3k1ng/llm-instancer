9a9335a5fca2f86f3dd510a8d5793bd719f499a2
n3k1ng <141495654+n3k1ng@users.noreply.github.com>
2024-12-09 21:30:13 +0100

    Add utils java file

---
+++ src/main/java/es/uma/Utils.java
@@ -0,0 +1,28 @@
+package es.uma;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Utils {
+
+    public static String readFile(String path) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(path)));
+        } catch (Exception e) {
+            System.err.println("Error reading file: " + path + " - " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void saveFile(String file, String path, String filename) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(file);
+            System.out.println("Response saved at " + path);
+        } catch (Exception e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+}
