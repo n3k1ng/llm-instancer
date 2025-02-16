ad1e4029f1ef43b4e0bb782916012d58bb2ceaa2
n3k1ng <141495654+n3k1ng@users.noreply.github.com>
2025-02-16 19:02:00 +0100

    Update Simple agent

--- src/main/java/es/uma/Simple/ISimple.java
+++ src/main/java/es/uma/Simple/ISimple.java
@@ -5,5 +5,25 @@ import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.V;
 
 public interface ISimple {
-    String chat(String message);
+    String system = """
+            Given a conceptual model in the UML-based Specification Environment and a syntax example for instance creation, your task is to generate valid instances that conform to the provided model.
+            ### Requirements
+            - Instances must be correctly formatted according to the syntax example.
+            - Avoid unnecessary commentary and output the instance in plain text (not markdown).
+            - Ensure strict adherence to the model's constraints, multiplicities, relatinoships and attributes.
+            - Provide multiple instances with diverse data values and structures.
+            - Additionally provide instances that cover edge cases for constraints, multiplicities, and attribute values.
+            """;
+
+    String message = """
+            Create instances for the following UML model:
+            {{modelUML}}
+
+            Here is an example of the language syntax for creating instances:
+            {{syntaxExample}} 
+            """;
+            
+    @SystemMessage(system)
+    @UserMessage(message)
+    String chat(@V("modelUML") String modelUML, @V("syntaxExample") String syntaxExample);
 }
--- src/main/java/es/uma/Simple/Main.java
+++ src/main/java/es/uma/Simple/Main.java
@@ -2,10 +2,20 @@ package es.uma.Simple;
 
 import es.uma.Agents;
 import es.uma.Models;
+import es.uma.Utils;
 
 public class Main {
     public static void main(String[] args) {
         ISimple simple = Agents.getSimple(Models.getModel("4o"));
-        System.out.println(simple.chat("Hello!"));
+        
+        // Read model and example
+        final String DIAGRAM = "hammers";
+        final String PROMPT_PATH = "./src/main/resources/prompts/" + DIAGRAM + "/";
+        String modelUML = Utils.readFile(PROMPT_PATH + "diagram.use"); 
+        String exampleSOIL = Utils.readFile(PROMPT_PATH + "examples/example_1.soil");
+
+        String response = simple.chat(modelUML, exampleSOIL);
+
+        System.out.println(response);
     }
 }
