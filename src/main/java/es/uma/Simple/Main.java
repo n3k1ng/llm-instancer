1f7df3e9d7c3297edfd7715894ef4ca988befaa9
n3k1ng <141495654+n3k1ng@users.noreply.github.com>
2025-02-16 21:32:13 +0100

    Add experiment logic to ISimple agent

--- src/main/java/es/uma/Simple/Main.java
+++ src/main/java/es/uma/Simple/Main.java
@@ -1,17 +1,16 @@
 package es.uma.Simple;
 
+import es.uma.Experiment;
 import es.uma.Llms;
 import es.uma.Utils;
 
 public class Main {
     public static void main(String[] args) {
+        Experiment experiment = new Experiment("bank");
         ISimple simple = Llms.getAgent(ISimple.class, Llms.getModel("4o"));
 
-        // Read model and example
-        final String DIAGRAM = "bank";
-        final String PROMPT_PATH = "./src/main/resources/prompts/" + DIAGRAM + "/";
-        String modelUML = Utils.readFile(PROMPT_PATH + "diagram.use"); 
-        String exampleSOIL = Utils.readFile(PROMPT_PATH + "examples/example_1.soil");
+        String modelUML = Utils.readFile(experiment.umlPath); 
+        String exampleSOIL = Utils.readFile(experiment.examplePath);
 
         String response = simple.chat(modelUML, exampleSOIL);
 
